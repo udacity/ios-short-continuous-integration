ef764b4af42da3d1bd16d5c4477c3eed481b7e6a
Jarrod Parkes <jarrod@udacity.com>
2016-12-06 10:48:50 -0600

    add example unit and ui tests

--- RomanNumeralCalculatorComplete/RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
+++ RomanNumeralCalculatorComplete/RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
@@ -7,29 +7,21 @@
 //
 
 import XCTest
+@testable import RomanNumeralCalculator
 
 class RomanNumeralCalculatorTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDecimalToRomanString() {
+        XCTAssertEqual(toRoman(1), "I")
+        XCTAssertEqual(toRoman(2), "II")
+        XCTAssertEqual(toRoman(4), "IV")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
--- RomanNumeralCalculatorComplete/RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorComplete/RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -9,28 +9,33 @@
 import XCTest
 
 class RomanNumeralCalculatorUITests: XCTestCase {
-        
+    
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        XCUIDevice.shared().orientation = .portrait
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAddOnePlusOne() {
+        // create app/element handles
+        let app = XCUIApplication()
+        let iButton = app.buttons["I"]
+        let calculatedLabel = app.staticTexts["calculated_value"]
+        // tap "I"
+        iButton.tap()
+        XCTAssertEqual(calculatedLabel.label, "I")
+        // tap "+"
+        app.buttons["+"].tap()
+        // tap "I"
+        iButton.tap()
+        XCTAssertEqual(calculatedLabel.label, "II")
     }
-    
 }
