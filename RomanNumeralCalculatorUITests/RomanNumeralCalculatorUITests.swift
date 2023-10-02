6300ab60d00fa2fc53d580fa9dd5dbdb30118f13
AhmedNafie <ahmedtareknafie@gmail.com>
2023-10-03 01:32:25 +0300

    add ui test to one plus one

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -8,28 +8,40 @@
 
 import XCTest
 
-class RomanNumeralCalculatorUITests: XCTestCase {
+@testable import RomanNumeralCalculator
 
+
+class RomanNumeralCalculatorUITests: XCTestCase {
     override func setUpWithError() throws {
+        let app = XCUIApplication()
+
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
+        XCUIDevice.shared.orientation = .portrait
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+            func testOnePlusOne() throws {
+                let app = XCUIApplication()
+                let iStaticText = app.buttons["I"].staticTexts["I"]
+                let calculatedLabel = app/*@START_MENU_TOKEN@*/.staticTexts["calculated_value"]/*[[".staticTexts[\"II\"]",".staticTexts[\"calculated_value\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+                //tap I
+                iStaticText.tap()
+                XCTAssertEqual(calculatedLabel.label, "I")
+                //tap +
+                app/*@START_MENU_TOKEN@*/.staticTexts["+"]/*[[".buttons[\"+\"].staticTexts[\"+\"]",".staticTexts[\"+\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+                // tap II
+                iStaticText.tap()
+                XCTAssertEqual(calculatedLabel.label, "II")
     }
 
     func testLaunchPerformance() throws {
